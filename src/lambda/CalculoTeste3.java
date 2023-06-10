e42d68cf54cbb106e522aaf08c9541ab5ba64326
AlexGuedesSiva <alex__guedes@hotmail.com>
2023-06-10 11:31:58 -0300

    update packate lambda

---
+++ src/lambda/CalculoTeste3.java
@@ -0,0 +1,26 @@
+package lambda;
+
+import java.util.function.BinaryOperator;
+
+public class CalculoTeste3 {
+    public static void main(String[] args) {
+
+        // NÃO: int -> Double
+        // double -> Double
+
+        BinaryOperator<Double> calc = Double::sum;
+        System.out.println(calc.apply(2.0, 3.0));
+
+        calc = (x, y) -> x * y;
+        System.out.println(calc.apply(2.0, 3.0));
+
+
+        BinaryOperator<Integer> calc2 = Integer::sum;
+        System.out.println(calc2.apply(2, 3));
+
+        calc2 = (x, y) -> x * y;
+        System.out.println(calc2.apply(2, 3));
+
+
+    }
+}
