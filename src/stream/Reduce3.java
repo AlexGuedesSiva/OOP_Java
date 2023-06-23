8f6791ca0e297cddcd4e917d973eac16cae14b8a
AlexGuedesSiva <alex__guedes@hotmail.com>
2023-06-23 07:13:53 -0300

    add algumas classes em stream

--- src/stream/Reduce3.java
+++ src/stream/Reduce3.java
@@ -1,16 +1,14 @@
 package stream;
 
-
-
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public class Reduce2 {
+public class Reduce3 {
     public static void main(String[] args) {
-
 
         Aluno a1 = new Aluno("Ana", 7.1);
         Aluno a2 = new Aluno("Luna", 6.1);
@@ -21,23 +19,19 @@ public class Reduce2 {
 
         Predicate<Aluno> aprovado = a -> a.nota >= 7;
         Function<Aluno, Double> apenasNota = a -> a.nota;
-        BinaryOperator<Double> somatorio = Double::sum;
 
-        alunos.stream()
+        BiFunction<Media, Double, Media> calcularMedia =
+                (media, nota) -> media.adicionar(nota);
+
+        BinaryOperator<Media> combinarMedia =
+                (m1,m2) -> Media.combinar(m1,m2);
+
+        Media media = alunos.parallelStream()
                 .filter(aprovado)
                 .map(apenasNota)
-                .reduce(somatorio)
-                .ifPresent(System.out::println);
+                .reduce(new Media(), calcularMedia, combinarMedia);
 
-
-
-
-
-
-
-
-
-
+        System.out.println("A média do aluno é " + media.getValor());
 
     }
 }
